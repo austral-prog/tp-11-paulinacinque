114da19e80e4c01105d22106847870101ba988e4
paulinacinque <pcinque1@mail.austral.edu.ar>
2025-07-11 13:57:39 -0300

    Update files_and_exceptions.py

--- files_and_exceptions.py
+++ files_and_exceptions.py
@@ -1,29 +1,30 @@
 def read_file_to_dict(filename):
-	ventas = {}			 
+    ventas = {}			 
     try:
-        with open(filename, “r”) as archivo:           
-		    contenido = archivo.read()          
-		    lista_entradas = contenido.split(‘;’)          
+        with open(filename, "r") as archivo:           
+            contenido = archivo.read()          
+            lista_entradas = contenido.split(';')          
 	
-	    	for entrada in lista_entradas:     
-		    	if entrada == ‘’:
-				    continue        
+            for entrada in lista_entradas:     
+                if entrada == '':
+                    continue        
 			
-            producto, valor = entrada.split(‘:’) 
-            valor = float(valor)  
+                producto, valor = entrada.split(':') 
+                valor = float(valor)  
 		
-		    	if producto not in ventas:
-				    ventas[producto] = [] 	       
-			    ventas[producto].append(valor)
+                if producto not in ventas:
+                    ventas[producto] = [] 	       
+                ventas[producto].append(valor)
 		
-	    return ventas
+        return ventas
     except FileNotFoundError:
-    	raise FileNotFoundError(f“El archivo ‘{filename}’ no existe”)
+        raise FileNotFoundError(f"El archivo '{filename}' no existe")
+
 
 def process_dict(data):
- 	for productos, lista in data.items():
-		total = 0
+    for producto, lista in data.items():
+        total = 0
         for valor in lista:
             total += valor
-            promedio = (total/len(lista))
-    print(f“{productos}: ventas totales ${total.2f}, promedio ${promedio.2f}”) 
+        promedio = total / len(lista)
+        print(f"{producto}: ventas totales ${total:.2f}, promedio ${promedio:.2f}")
